54dc3e7e2c76168fbd7220f510d033a35ae3ea1f
Grzegorz <grzegorzdziedzic198@gmail.com>
2023-08-25 23:04:50 +0200

    build:
    Add types to packages to have it common in apps

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,6 +2,13 @@ import { createServer } from "./server";
 import { log } from "logger";
 import dotenv from "dotenv";
 dotenv.config()
+import {User} from 'types'
+const user1: User ={
+    id: '1',
+    email: '1',
+    password: '1'
+}
+console.log(user1)
 
 const port = process.env.PORT || 5001;
 const server = createServer();
