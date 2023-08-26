70763ff224acff980750e700f83335777cdad1b5
Grzegorz <grzegorzdziedzic198@gmail.com>
2023-08-26 23:02:43 +0200

    feat(Frontend)!: Map
    BREAKING CHANGE: leaflet Map

--- apps/frontend/src/components/Map/Map.tsx
+++ apps/frontend/src/components/Map/Map.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 import './Map.css'
-
+import {MapContainer, Marker, TileLayer} from "react-leaflet";
+import 'leaflet/dist/leaflet.css'
 export function Map() {
     return (
         <div className="map">
+            <MapContainer center={[51.9412722,15.5007792]} zoom={13} scrollWheelZoom={true}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution="&copy; <a href=\'https://www.openstreetmap.org/\'>OpenStreetMap</a> contributors"
+                />
+                <Marker position={[51.9412722,15.5007792]}>
+
+                </Marker>
+            </MapContainer>
+
         </div>
     )
 }
