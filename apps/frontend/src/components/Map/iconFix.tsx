944787f35f04f5944c080649feb9d7d49bf5d58e
Grzegorz <grzegorzdziedzic198@gmail.com>
2023-09-06 23:57:07 +0200

    fix(Frontend): issue with display Icon on the map

---
+++ apps/frontend/src/components/Map/iconFix.tsx
@@ -0,0 +1,14 @@
+import L from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import iconRetina from 'leaflet/dist/images/marker-icon-2x.png';
+
+const DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow,
+    iconRetinaUrl: iconRetina,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
